c2255c4e6b47abcb310011b5b2efe68f9b4c0e17
sandoya1 <83371887+sandoya1@users.noreply.github.com>
2021-06-27 18:36:01 -0500

    Delete La estructura REPEAT.py

--- La estructura REPEAT.py
+++
@@ -1,7 +0,0 @@
-#Isaias Rafael Sandoya Vargas 
-#Software A1
-
-#BUCLE REPEAT CONTROLADO POR BANDERAS.
-#EJEMPLO 1
-#Aplicar los pasos de la metodología para la solución de un problema para leer un número entero N y calcular el resultado de la siguiente serie:
-#1 – 1/2+ 1/3 – 1/4 +.... +/- 1/N. Resolveremos el problema utilizando bucle Repeat controlado por contador y usando banderas.
